3d6f8405892512eb0c485e2c9dd4e8e9ab109786
daniele-andreotti <daniele.andreotti@redturtle.it>
2017-02-03 14:14:48 +0100

    Refactoring

--- rer/groupware/core/groupware_selection_criterion.py
+++ rer/groupware/core/groupware_selection_criterion.py
@@ -1,54 +1,33 @@
-from Products.ATContentTypes.criteria import LIST_INDICES, registerCriterion
-from Products.ATContentTypes.criteria.selection import \
-    ATSelectionCriterionSchema, ATSelectionCriterion
-from Products.ATContentTypes.interfaces import IATTopicSearchCriterion
-from Products.Archetypes.atapi import DisplayList
 from Products.CMFCore.utils import getToolByName
-from rer.groupware.core.interfaces import IATGroupwareSelectionCriteria
-from zope.interface import implements
 from rer.groupware.core import groupwarecoreMessageFactory as _
 
-CompareOperators = DisplayList((
-                    ('and', _(u'and'))
-                  , ('or', _(u'or'))
-    ))
 
 
-class ATGroupwareSelectionCriterion(ATSelectionCriterion):
-    """A selection criterion"""
+def getCurrentValues(context, row):
+    options = getFilteredSubjects(context)
 
-    implements(IATGroupwareSelectionCriteria, IATTopicSearchCriterion)
-    schema = ATSelectionCriterionSchema
-    meta_type = 'ATGroupwareSelectionCriterion'
-    archetype_name = 'Groupware Selection Criterion'
-    shortDesc = 'Select values from list filtered by room'
+    options = [(o.lower(),o) for o in options if isinstance(o, basestring)]
+    options.sort()
 
-    def getCurrentValues(self):
-        
-        options = self.getFilteredSubjects()
-        # AT is currently broken, and does not accept ints as
-        # DisplayList keys though it is supposed to (it should
-        # probably accept Booleans as well) so we only accept strings
-        # for now
-        options = [(o.lower(),o) for o in options if isinstance(o, basestring)]
-        options.sort()
-        return [o[1] for o in options]
+    res = [o[1] for o in options]
+    return {row.index: {'query': res, }}
 
-    def getFilteredSubjects(self):
-        """
-        Return a list of keywords for the current room
-        """
-        catalog = getToolByName(self, 'portal_catalog')
-        room=None
-        for elem in self.aq_chain:
-            if getattr(elem,'portal_type','') == 'GroupRoom':
-                room = elem
-        if not room:
-            return catalog.uniqueValuesFor(self.Field())
-        room_elements=catalog(path='/'.join(room.getPhysicalPath()))
-        subjects=set()
-        for brain in room_elements:
-            subjects.update(getattr(brain,self.Field(),()))
-        return tuple(subjects)
-        
-registerCriterion(ATGroupwareSelectionCriterion, LIST_INDICES)
+
+def getFilteredSubjects(context):
+    """
+    Return a list of keywords for the current room
+    """
+    catalog = getToolByName(context, 'portal_catalog')
+    room = None
+
+    for elem in context.aq_chain:
+        if getattr(elem, 'portal_type', '') == 'GroupRoom':
+            room = elem
+    if not room:
+        return catalog.uniqueValuesFor(context.Field())
+    room_elements = catalog(path='/'.join(room.getPhysicalPath()))
+    subjects = set()
+    for brain in room_elements:
+        subjects.update(getattr(brain, 'Subject', ()))
+
+    return tuple(subjects)
